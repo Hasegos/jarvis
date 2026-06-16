fcafadf64f8319d2167252293dc42ee740daaa1b
Hasegos <csw020106@naver.com>
2026-06-16 22:52:46 +0900

    FIX EffectComposer.js 수정
    삭제된 _geometry 변수값 추가

--- static/lib/three/EffectComposer.js
+++ static/lib/three/EffectComposer.js
@@ -140,6 +140,7 @@
 		}
 	} // Helper for passes that need to fill the viewport with a single quad.
 	const _camera = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 ); // https://github.com/mrdoob/three.js/pull/21358
+	const _geometry = new THREE.BufferGeometry();
 	_geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( [ - 1, 3, 0, - 1, - 1, 0, 3, - 1, 0 ], 3 ) );
 	_geometry.setAttribute( 'uv', new THREE.Float32BufferAttribute( [ 0, 2, 0, 0, 2, 0 ], 2 ) );
 	class FullScreenQuad {
